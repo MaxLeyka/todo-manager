356aef058cedd579f3379801e7c6b77d1f4eaf8a
Lazarenko Maxim <maxlaz435@gmail.com>
2025-11-19 18:04:04 +0300

    feat: Создан роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TaskList from 'app/TaskList/TaskList';
+import AddTask from 'app/AddTask/AddTask';
+import NotFound from 'app/NotFound/NotFound';
+import EditTask from 'app/EditTask/EditTask';
 
 const App = () => {
   return (
-    <div>
-      <h1>todo app</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/add" element={<AddTask />} />
+        <Route path="/edit/:taskId" element={<EditTask />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
