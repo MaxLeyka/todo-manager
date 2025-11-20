1bb1bb2ede55a5d58844a31c195e9585ac1f7ad0
Lazarenko Maxim <maxlaz435@gmail.com>
2025-11-20 17:54:36 +0300

    feat: Создана основная страница сайта с выводом списка всех записей и возможностью быстрого изменения некоторых параметров отдельной записи (выполнение, важность)

--- src/app/TaskList/TaskList.tsx
+++ src/app/TaskList/TaskList.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDeleteTaskMutation, useGetAllTasksQuery, useUpdateTaskMutation } from 'src/api/tasksApi';
+import { TaskFilterParams, UpdateTask } from 'types/task';
+import TaskFilter from 'app/TaskList/components/TaskFilter/TaskFilter';
+import TaskItem from 'app/TaskList/components/TaskItem/TaskItem';
 
 const TaskList = () => {
-  return <div>list</div>;
+  const [filter, setFilter] = useState<TaskFilterParams>({});
+  const { data } = useGetAllTasksQuery(filter);
+  const [patchTask] = useUpdateTaskMutation();
+  const [deleteTask] = useDeleteTaskMutation();
+
+  const handleToggleImportant = (taskId: number, data: UpdateTask) => {
+    patchTask({
+      id: taskId,
+      data: { ...data, isImportant: !data.isImportant },
+    });
+  };
+  const handleToggleCompleted = (taskId: number, data: UpdateTask) => {
+    patchTask({
+      id: taskId,
+      data: { ...data, isCompleted: !data.isCompleted },
+    });
+  };
+  const handleDelete = (taskId: number) => {
+    deleteTask(taskId);
+  };
+  return (
+    <div>
+      {data?.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onDelete={handleDelete}
+          onToggleImportant={() => handleToggleImportant(task.id, task)}
+          onToggleCompleted={() => handleToggleCompleted(task.id, task)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TaskList;
