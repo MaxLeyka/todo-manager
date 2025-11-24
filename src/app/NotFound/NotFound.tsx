fde3194964cbbae1236fd0515cd0505a4a14b0f4
Lazarenko Maxim <maxlaz435@gmail.com>
2025-11-24 14:24:40 +0300

    feat: Создана не стилизованная версия 404 страницы (с доступностью)

--- src/app/NotFound/NotFound.tsx
+++ src/app/NotFound/NotFound.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => {
-  return <div>404</div>;
+const NotFound: React.FC = () => {
+  return (
+    <main aria-labelledby="not-found-heading">
+      <h1 id="not-found-heading">Ошибка 404 - страница не найдена</h1>
+      <p>Запрашиваемая страница не существует.</p>
+      <Link to="/">
+        <button>Перейти на главную</button>
+      </Link>
+    </main>
+  );
 };
 
 export default NotFound;
