c59a488838c2e7cfe3fd5e019304df074b262533
Lazarenko Maxim <maxlaz435@gmail.com>
2025-11-25 14:06:19 +0300

    feat: Добавлен кастомный мидлвейр для логгирования и упрощения дебаггинга

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { loggerMiddleware } from './middleware/loggerMiddleware';
 import { tasksApi } from 'api/tasksApi';
 
 export const store = configureStore({
   reducer: {
     [tasksApi.reducerPath]: tasksApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tasksApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tasksApi.middleware).concat(loggerMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
