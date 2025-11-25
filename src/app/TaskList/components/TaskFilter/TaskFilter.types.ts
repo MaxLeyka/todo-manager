86957e5ab0e7cad78de90d61e2f7f042777f041f
Lazarenko Maxim <maxlaz435@gmail.com>
2025-11-25 14:48:29 +0300

    feat: Вынесен интерфейс для фильтра

--- src/app/TaskList/components/TaskFilter/TaskFilter.types.ts
+++ src/app/TaskList/components/TaskFilter/TaskFilter.types.ts
@@ -3,3 +3,9 @@ import { TaskFilterParams } from 'types/task';
 export interface TaskFilterProps {
   onFilterChange: (filters: TaskFilterParams) => void;
 }
+
+export interface FilterFormData {
+  name_like: string;
+  isCompleted: 'all' | 'completed' | 'active';
+  isImportant: 'all' | 'important' | 'not-important';
+}
